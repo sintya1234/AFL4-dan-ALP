96842e4d183264925a39eb887793ea6c138a8496
Sintya <sriwahyu@student.ciputra.ac.id>
2022-05-24 09:01:25 +0700

    Initial Commit

---
+++ AFL4 dan ALP/AFL4_dan_ALPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL4_dan_ALPApp.swift
+//  AFL4 dan ALP
+//
+//  Created by SIFT-DBT Telkom Air 10 on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL4_dan_ALPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL4 dan ALP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AFL4 dan ALP
+//
+//  Created by SIFT-DBT Telkom Air 10 on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
